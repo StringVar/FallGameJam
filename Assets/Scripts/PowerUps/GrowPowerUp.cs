d35f05d710dfa3c2a80412b019d3be23c9ccfeee
Evan Funnell <funnelle@mymacewan.ca>
2018-11-11 01:35:30 -0700

    Added Grow Power Script

---
+++ Assets/Scripts/PowerUps/GrowPowerUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowPowerUp : MonoBehaviour {
+    [SerializeField] private float activeTime;
+
+    public void Effect() {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            Effect();
+        }
+    }
+
+    private IEnumerator PowerUpLength(float powerUpTime) {
+        yield return new WaitForSeconds(powerUpTime);
+    }
+}

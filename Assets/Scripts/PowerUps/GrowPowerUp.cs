b2bf29078d4775910458774e99c57f624a5b72b5
Evan Funnell <funnelle@mymacewan.ca>
2018-11-11 15:49:51 -0700

    Added power up code

--- Assets/Scripts/PowerUps/GrowPowerUp.cs
+++ Assets/Scripts/PowerUps/GrowPowerUp.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class GrowPowerUp : MonoBehaviour {
     [SerializeField] private float activeTime;
+    private GameObject player;
+    private Vector2 defaultScale;
+
+    private void Start() {
+        defaultScale = player.transform.localScale;
+    }
 
     public void Effect() {
-
+        player.transform.localScale = new Vector2(2, 2);
+        StartCoroutine(PowerUpLength(activeTime));
     }
 
     public void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player") {
+            player = collision.gameObject;
             Effect();
         }
     }
 
     private IEnumerator PowerUpLength(float powerUpTime) {
         yield return new WaitForSeconds(powerUpTime);
+        player.transform.localScale = defaultScale;
     }
 }
--- Assets/Scripts/PowerUps/ShrinkPowerUp.cs
+++ Assets/Scripts/PowerUps/ShrinkPowerUp.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class ShrinkPowerUp : MonoBehaviour {
     [SerializeField] private float activeTime;
+    private GameObject player;
+    private Vector2 defaultScale;
+
+    private void Start() {
+        defaultScale = player.transform.localScale;
+    }
 
     public void Effect() {
-
+        player.transform.localScale = new Vector2(0.5f, 0.5f);
+        StartCoroutine(PowerUpLength(activeTime));
     }
 
     public void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player") {
+            player = collision.gameObject;
             Effect();
         }
     }
 
     private IEnumerator PowerUpLength(float powerUpTime) {
         yield return new WaitForSeconds(powerUpTime);
+        player.transform.localScale = defaultScale;
     }
 }
+

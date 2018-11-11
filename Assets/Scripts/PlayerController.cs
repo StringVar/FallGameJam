15b81fa0ab04b9cfc39ce5bf0797a35c27e89d02
Cameron White <cameron.ag.white@gmail.com>
2018-11-10 21:27:32 -0700

    Yup there is input now

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlatformerController))]
+public class PlayerController : MonoBehaviour 
+{	
+	public GamepadInputModule gamepad;
+
+	public PlatformerController controller;
+	
+	
+	void Start ()
+	{
+		controller = GetComponent<PlatformerController>();
+	}
+	
+	void Update () {
+		controller.Move(gamepad.LeftAxis);
+
+	}
+}
